96899ff45693525a901883227349b0becdcaf94a
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2021-04-21 12:41:07 +0530

    10 Days of Javascript HackerRank Solutions Created

---
+++ Day 1: Let and Const.js
@@ -0,0 +1,9 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r= readLine()
+    // Print the area of the circle:
+    var area = Math.PI*r*r;
+    console.log(area)
+    // Print the perimeter of the circle:
+    var perimeter = 2*Math.PI*r;
+    console.log(perimeter)
